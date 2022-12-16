74d85cbc7990439ec38bfad2faa20a8c5ce6aea6
Aaron Wallis <aaron.wallis@lexer.com.au>
2022-12-17 08:01:29 +1100

    Added icon

---
+++ src/shared/icon.ts
@@ -0,0 +1,23 @@
+import { css, CSSResultGroup, html, LitElement, TemplateResult } from "lit";
+import { property, customElement } from "lit/decorators.js";
+
+@customElement("muto-icon")
+export class MutoIcon extends LitElement {
+    @property() public icon: string | undefined = undefined;
+
+    protected render(): TemplateResult {
+        return html`
+            <div class="muto-icon">
+                <ha-icon .icon=${this.icon}></ha-icon>
+            </div>
+        `;
+    }
+
+    static get styles(): CSSResultGroup {
+        return css`
+            :host {
+                display: inline-block;
+            }
+        `;
+    }
+}
